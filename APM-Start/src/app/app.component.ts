a30b008ad9582d3d5dec94b764eb059c92fd5069
shaanthenapster <sanu.bhattacharya@tothenew.com>
2018-06-13 16:34:23 +0530

    altered own comppnent

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  <div><h1>{{title}}</h1>
+    <div> the component is now ready </div>
+  
+  </div>
+   `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title = 'Angular shuru hua ';
 }
